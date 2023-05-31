8b38b3459bd6ae50c1c9041aba9c47c3a77b5672
kmanthrige <k.manthrige@gmail.com>
2023-05-31 16:08:15 -0500

    price changed

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import EditButton from "./EditButton.js"
 export default function App() {
 
   const grocertyItems = [
-    { id: 1, item: "apples", cost: "$1.99", quantity: 5 },
-    { id: 2, item: "bread", cost: "$2.99", quantity: 2 },
+    { id: 1, item: "apples", cost: "$2.99", quantity: 5 },
+    { id: 2, item: "bread", cost: "$3.99", quantity: 2 },
     { id: 3, item: "beans", cost: "$0.99", quantity: 4 },
     { id: 4, item: "onions", cost: "$1.89", quantity: 1 },
     { id: 5, item: "tomatoes", cost: "$0.50", quantity: 2 }
